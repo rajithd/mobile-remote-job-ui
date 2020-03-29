f68dc1ac977a15f8a36580d494cf1ee9daeba692
Rajith Delantha <rajith.delantha@aurea.com>
2020-03-29 11:49:04 +0530

    introduce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { Platform, Nav } from "ionic-angular";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Keyboard } from '@ionic-native/keyboard';
+
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { LocalWeatherPage } from "../pages/local-weather/local-weather";
+
+export interface MenuItem {
+    title: string;
+    component: any;
+    icon: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: 'app.html'
 })
-export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
+
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  appMenuItems: Array<MenuItem>;
 
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    public keyboard: Keyboard
   ) {
     this.initializeApp();
+
+    this.appMenuItems = [
+      {title: 'Home', component: HomePage, icon: 'home'},
+      {title: 'Local Weather', component: LocalWeatherPage, icon: 'partly-sunny'}
+    ];
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+
+      //*** Control Splash Screen
+      // this.splashScreen.show();
+      // this.splashScreen.hide();
+
+      //*** Control Status Bar
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.statusBar.overlaysWebView(false);
+
+      //*** Control Keyboard
+      //this.keyboard.disableScroll(true);
     });
   }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  logout() {
+    this.nav.setRoot(LoginPage);
+  }
+
 }
